a813089f37ecb9f01725ef003132d4bfe7807608
Carlos Mota <carloseduardomotapro@gmail.com>
2026-09-10 22:48:03 -0300

    cadastro de notas

--- aluno.py
+++ aluno.py
@@ -5,3 +5,13 @@ class Aluno:
 
     def adicionar_nota(self, nota):
         self.notas.append(nota)
+
+
+if __name__ == "__main__":
+    nome = input("Nome do aluno: ")
+    aluno = Aluno(nome)
+
+    nota1 = float(input("Nota 1: "))
+    nota2 = float(input("Nota 2: "))
+    aluno.adicionar_nota(nota1)
+    aluno.adicionar_nota(nota2)
